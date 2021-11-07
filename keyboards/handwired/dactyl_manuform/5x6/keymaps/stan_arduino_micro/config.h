0551acf23cb7d4f02d40857482964426f27ffbb5
Stanley Brown <stan.j.brown@gmail.com>
2021-11-07 16:28:20 -0500

    add config files for dactyle manuform with micro and pro micro boards

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/stan_arduino_micro/config.h
@@ -0,0 +1,35 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+
+// legit ardino micro has odd pinouts, d3 worked best
+#define SOFT_SERIAL_PIN D3
+#define SELECT_SOFT_SERIAL_SPEED 4
+
+#define SPLIT_USB_DETECT
+#define MASTER_LEFT
+
+// #define MASTER_RIGHT
+//#define EE_HANDS
+// Rows are doubled-up
